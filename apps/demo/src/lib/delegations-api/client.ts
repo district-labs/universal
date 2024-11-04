5c6fdbe0c95f8970e2a37053a14d17457915d7a9
Vitor <marthendalnunes@gmail.com>
2024-11-04 13:26:00 -0300

    feat: add delegations api actions

--- apps/demo/src/lib/delegations-api/client.ts
+++ apps/demo/src/lib/delegations-api/client.ts
@@ -1,4 +1,4 @@
-import { hc } from 'hono/client'
-import type { AppRouter } from 'delegations-api'
+import { hc } from "hono/client";
+import type { AppRouter } from "delegations-api";
 
-export const delegationsAPiClient = hc<AppRouter>('http://localhost:8787/')
+export const delegationsAPiClient = hc<AppRouter>("http://localhost:8787/");
