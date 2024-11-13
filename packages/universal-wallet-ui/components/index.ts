5a2fec7460822f198ce6abd727f646e47990fbd4
Kames <sudo@kames.me>
2024-11-13 07:25:42 -0700

    wip: credential issuance

--- packages/universal-wallet-ui/components/index.ts
+++ packages/universal-wallet-ui/components/index.ts
@@ -2,4 +2,9 @@ export * from './finance/debit-card';
 export * from './icon-ethereum';
 export * from './icon-loading';
 export * from './identity/did-card';
+export * from './svg-icon';
+// export { FarcasterIcon, GithubIcon, XIcon };
+//     import FarcasterIcon from './assets/brands/farcaster.svg';
+//     import GithubIcon from './assets/brands/github.svg';
+//     import XIcon from './assets/brands/x.svg';
 
