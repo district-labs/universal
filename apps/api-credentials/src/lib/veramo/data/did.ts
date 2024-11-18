ceaa55f00acc2806a4a51f3d49dacffb0cac114d
Vitor <marthendalnunes@gmail.com>
2024-11-18 14:51:56 -0300

    fix: credentials api did url

--- apps/api-credentials/src/lib/veramo/data/did.ts
+++ apps/api-credentials/src/lib/veramo/data/did.ts
@@ -1,4 +1,10 @@
-import { alias } from "../agent.js";
+import { alias } from '../agent.js';
+
+if (!process.env.DID_PUB_KEY) {
+  throw new Error('DID_PUB_KEY env var is required');
+}
+
+const didPubKey = process.env.DID_PUB_KEY;
 
 export const webDid = {
   '@context': [
@@ -12,8 +18,7 @@ export const webDid = {
       id: `did:web:${alias}#04ed446cfec055f86e568a9b819399415df8f3d755a410754edc858c0d9b5b688e8d9a4bb1853f8ca1d80f20fa3eacbea2e198bb73d8cf2e38bbf6e0ee7779c5dc`,
       type: 'EcdsaSecp256k1VerificationKey2019',
       controller: `did:web:${alias}`,
-      publicKeyHex:
-        '04ed446cfec055f86e568a9b819399415df8f3d755a410754edc858c0d9b5b688e8d9a4bb1853f8ca1d80f20fa3eacbea2e198bb73d8cf2e38bbf6e0ee7779c5dc',
+      publicKeyHex: didPubKey,
     },
   ],
   authentication: [
