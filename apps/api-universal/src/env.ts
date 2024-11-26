65004294343a6cceec813791ea9d04f4f591d587
Vitor <marthendalnunes@gmail.com>
2024-11-26 13:31:43 -0300

    fix: load env variables on hono

--- apps/api-universal/src/env.ts
+++ apps/api-universal/src/env.ts
@@ -1,9 +1,10 @@
 import { createEnv } from '@t3-oss/env-core';
 import { z } from 'zod';
+import 'dotenv/config';
 
 export const env = createEnv({
   server: {
-    UNIVERSAL_DATABASE_URL: z.string().url(),
+    UNIVERSAL_DATABASE_URL: z.string().url().default('http://localhost:4200/'),
     CREDENTIALS_API_URL: z.string().url().default('http://localhost:3100/'),
     DELEGATIONS_API_URL: z.string().url().default('http://localhost:3200/'),
     IDENTITY_API_URL: z.string().url().default('http://localhost:3300/'),
