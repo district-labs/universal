812a668e4ddd6aa39b862bf1f596204de34d25c5
Vitor <marthendalnunes@gmail.com>
2024-12-02 22:52:40 -0300

    chore: add mainnet back to wc

--- apps/wallet/src/lib/walletconnect/constants.ts
+++ apps/wallet/src/lib/walletconnect/constants.ts
@@ -1,6 +1,8 @@
 import { productionChainIds, testnetChainIds } from 'universal-data';
+import { mainnet } from 'viem/chains';
 
 export const supportedChainIdsWc = [
+  mainnet.id,
   ...productionChainIds,
   ...testnetChainIds,
 ] as const;
